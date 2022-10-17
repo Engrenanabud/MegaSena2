e4a994b56727bd05564559174c25b5419cea4ad2
Renan Abud <abudguitarrista@gmail.com>
2022-10-17 10:30:42 -0300

    painelcommit

---
+++ PainelEstudo/src/PainelEstudo.java
@@ -0,0 +1,16 @@
+public class PainelEstudo {
+    public static void main (String[] args){
+
+        //funções primitivas
+                boolean isLogged = true;
+                byte b = 'a';
+                char c = 'c';
+                short s = -129; // =int (só que com menos espaço)
+                int i = 123445; //grande
+                long u = 12323245; //igual int só que com mais espaço
+                float f = 19.19f; // 32bits (menor do que double)
+                double d = 19.19; // 64 bits
+
+    }
+
+}
